0356262dd4654e4edb05c90c3d46a7f2155092b0
Melissa Shao <mshao8@my.bcit.ca>
2024-07-11 13:16:37 -0700

    Add animation for the brain svg

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,13 @@
 "use client";
-import { motion } from "framer-motion";
 import Brain from "@/components/brain";
+import { motion, useScroll } from "framer-motion";
+import { useRef } from "react";
 
 const Aboutpage = () => {
+  const containerRef = useRef();
+  const { scrollYProgress } = useScroll({ container: containerRef });
+  console.log(scrollYProgress);
+
   return (
     <motion.div className="h-full"
       initial={{ y: "-200vh" }}
@@ -10,7 +15,7 @@ const Aboutpage = () => {
       transition={{ duration: 1 }}>
 
       {/* container */}
-      <div className="h-full overflow-scroll lg:flex">
+      <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
 
         {/* text container */}
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:1/2">
@@ -173,8 +178,8 @@ const Aboutpage = () => {
         </div>
 
         {/* svg container */}
-        <div className="hidden lg:block w-1/3 xl:1/2 sticky top-0 z-30">
-          <Brain />
+        <div className="hidden lg:block w-1/3 xl:w-1/2 sticky top-0 z-30">
+          <Brain scrollYProgress={scrollYProgress} />
         </div>
       </div>
     </motion.div>
