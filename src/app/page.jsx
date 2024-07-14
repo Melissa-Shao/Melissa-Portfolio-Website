cba6a5eeb33764c567568a20c22379188e11a323
Melissa Shao <mshao8@my.bcit.ca>
2024-07-14 00:12:18 -0700

    Add button links for homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Homepage = () => {
 
@@ -24,8 +25,12 @@ const Homepage = () => {
           <p className="md:text-xl">Welcome to my digital canvas, where innovation and creativity converge. With a keen eye for aesthetics and a mastery of code, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.</p>
           {/* buttons */}
           <div className="flex gap-4 w-full">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View my work</button>
-            <button className="p-4 rounded-lg ring-1 ring-black ">Contact Me</button>
+            <Link href="/portfolio">
+              <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View my work</button>
+            </Link>
+            <Link href="/contact">
+              <button className="p-4 rounded-lg ring-1 ring-black ">Contact Me</button>
+            </Link>
           </div>
         </div>
       </div>
