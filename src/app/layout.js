2ea25ea444bf0e6e7b7b3d7b7794caf976ef2113
Melissa Shao <mshao8@my.bcit.ca>
2024-07-14 00:48:01 -0700

    Add favicon for the app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Head from 'next/head';
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
 
@@ -12,6 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+      </Head>
       <body className={inter.className}>
         <TransitionProvider>
           {children}
