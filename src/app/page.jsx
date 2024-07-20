f562fef9ccaaff29b812fe3c280ed0e5abb8da2f
Melissa Shao <mshao8@my.bcit.ca>
2024-07-19 23:49:23 -0700

    Rewrite the content for the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -22,7 +22,7 @@ const Homepage = () => {
           {/* title */}
           <h1 className="text-4xl md:text-6xl font-bold">Crafting Digital Experiences, Designing Tomorrow.</h1>
           {/* desc */}
-          <p className="md:text-xl">Welcome to my digital canvas, where innovation and creativity converge. With a keen eye for aesthetics and a mastery of code, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.</p>
+          <p className="md:text-xl">Welcome to my digital canvas, where innovation, creativity, and technical expertise converge. With a keen eye for aesthetics, a mastery of code, and a deep understanding of data, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.</p>
           {/* buttons */}
           <div className="flex gap-4 w-full">
             <Link href="/portfolio">
