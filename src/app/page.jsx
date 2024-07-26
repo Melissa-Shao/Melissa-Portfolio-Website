ab5f32396a724e9dbe2fc9f9b9f4c0e08a023675
Melissa Shao <mshao8@my.bcit.ca>
2024-07-25 21:29:13 -0700

    Adjust the font size for small screen

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,15 +15,15 @@ const Homepage = () => {
 
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* image container */}
-        <div className="h-1/2 relative lg:h-full lg:w-1/2">
+        <div className="h-2/5 relative lg:h-full lg:w-1/2">
           <Image src="/woman.png" alt="" fill className="object-contain" />
         </div>
         {/* text container */}
         <div className="h-1/2 flex flex-col lg:h-full lg:w-1/2 gap-8 items-center justify-center">
           {/* title */}
-          <h1 className="text-4xl md:text-6xl font-bold">Crafting Digital Experiences, Designing Tomorrow.</h1>
+          <h1 className="text-3xl md:text-6xl font-bold">Crafting Digital Experiences, Designing Tomorrow.</h1>
           {/* desc */}
-          <p className="md:text-xl">Welcome to my digital canvas, where innovation, creativity, and technical expertise converge. With a keen eye for aesthetics, a mastery of code, and a deep understanding of data, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.</p>
+          <p className="md:text-lg">Welcome to my digital canvas, where innovation, creativity, and technical expertise converge. With a keen eye for aesthetics, a mastery of code, and a deep understanding of data, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.</p>
           {/* buttons */}
           <div className="flex gap-4 w-full">
             <Link href="/portfolio">
