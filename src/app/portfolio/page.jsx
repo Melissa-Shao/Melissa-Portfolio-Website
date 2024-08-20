10534edbd20e8621a664d43538bbe63688c596fd
Melissa Shao <mshao8@my.bcit.ca>
2024-08-20 01:24:03 -0700

    Add the project content for portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -3,40 +3,47 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Contactpage from "../contact/page";
 
 const items = [
   {
     id: 1,
     color: "from-red-300 to-blue-300",
-    title: "React Commerce",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: "https://lama.dev",
+    title: "SmileEstate App",
+    desc: "SmileEstate is a platform designed to help people find and post properties for sale or rent, with a real-time chat feature to enhance communication efficiency between both parties.",
+    img: "/smile.png",
+    link: "https://smile-estate-app.onrender.com",
   },
   {
     id: 2,
     color: "from-blue-300 to-violet-300",
-    title: "Next.js Medium Blog",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: "https://lama.dev",
+    title: "Dronify App",
+    desc: "Dronify is a web application that deploys autonomous drones (on-demand) to offer package delivery and roadside assistance for autonomous car owners during accidents, thereby reducing delivery time and traffic congestion.",
+    img: "/dronify.png",
+    link: "https://dronify-mbme.onrender.com/",
   },
   {
     id: 3,
     color: "from-violet-300 to-purple-300",
-    title: "Vanilla Book App",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: "https://lama.dev",
+    title: "ProcrastinHate App",
+    desc: "Developed an innovative application designed to help users tackle procrastination by providing tools and resources to improve their physical and mental health. Encourage users to stay focused and productive, and improve their happiness and well-being.",
+    img: "/1.png",
+    link: "https://dtc11-project.web.app",
   },
   {
     id: 4,
     color: "from-purple-300 to-red-300",
-    title: "Spotify Music App",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    link: "https://lama.dev",
+    title: "Pikachu Game",
+    desc: "Pikachu game is a text-based single-user dungeongame that allows users to play as Pikachu and interact with other characters in the game. The game is designed to be fun and engaging for users of all ages.",
+    img: "/pikachu.png",
+    link: "https://github.com/candiceweily/1510_team_project.git",
+  },
+  {
+    id: 5,
+    color: "from-red-300 to-blue-300",
+    title: "Data Analysis Project",
+    desc: "This project involves a data analysis workflow using Kaggle API for data acquisition, followed by data management and analysis using Python, Pandas, and SQL.",
+    img: "/data.png",
+    link: "https://github.com/Melissa-Shao/data-analysis-project.git",
   },
 ];
 
@@ -51,19 +58,19 @@ const Portfoliopage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}>
       <div className="h-[600vh] relative" ref={ref}>
-        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">My Works</div>
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-6xl md:text-8xl lg:text-8xl text-center">My Projects</div>
         <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
           <motion.div style={{ x }} className="flex ">
             <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300"></div>
 
             {items.map(item => (<div className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`} key={item.id}>
               <div className="flex flex-col gap-8 text-white">
-                <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">{item.title}</h1>
-                <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
+                <h1 className="text-3xl font-bold md:text-4xl lg:text-4xl xl:text-5xl xl:pt-8">{item.title}</h1>
+                <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[500px] xl:h-[350px]">
                   <Image src={item.img} alt='' fill />
                 </div>
-                <p className=" w-80 md:w-96 lg:w-[500px] lg:text-lg xl:w-[600px]">{item.desc}</p>
-                <Link href={item.link} className="flex justify-end"><button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold rounded m-4">See Demo</button></Link>
+                <p className=" w-80 md:w-96 lg:w-[500px] text-xl md:text-lg lg:text-lg xl:w-[500px]">{item.desc}</p>
+                <Link href={item.link} target="_blank" rel="noopener noreferrer" className="flex justify-end"><button className="p-2 text-lg md:p-4 md:text-md lg:p-4 lg:text-lg bg-white text-gray-600 font-semibold rounded m-2">See Demo</button></Link>
               </div>
             </div>
             ))}
@@ -71,8 +78,8 @@ const Portfoliopage = () => {
         </div>
       </div>
 
-      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center bg-gradient-to-t from-yellow to-purple">
-        <h1 className="text-8xl">Do you have project?</h1>
+      <div className="w-screen h-screen flex flex-col items-center justify-center text-center bg-gradient-to-t from-yellow to-purple">
+        <h1 className="text-6xl md:text-6xl lg:text-8xl">Want to contact me?</h1>
         {/*  portfolio svg  */}
         <div className="relative">
 
@@ -94,7 +101,7 @@ const Portfoliopage = () => {
               </textPath>
             </text>
           </motion.svg>
-          <Link href="/contact" className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center">Hire Me
+          <Link href="/contact" className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center">Contact Me
           </Link>
         </div>
       </div>
